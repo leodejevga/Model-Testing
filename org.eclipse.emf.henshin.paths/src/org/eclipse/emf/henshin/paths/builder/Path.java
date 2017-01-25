283c31bf2e53ffb4a9ea1557b9cf400b1ed3b57d
Jevgenij Huebert <huebertj@students.uni-marburg.de>
2017-01-25 12:36:11 +0100

    neuer prototyp

--- org.eclipse.emf.henshin.paths/src/org/eclipse/emf/henshin/paths/builder/Path.java
+++ org.eclipse.emf.henshin.paths/src/org/eclipse/emf/henshin/paths/builder/Path.java
@@ -1,5 +1,6 @@
 package org.eclipse.emf.henshin.paths.builder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,43 +10,62 @@ import org.eclipse.core.resources.IProject;
 
 public class Path {
 	private IProject project;
-	private Map<String, String> entries;
+	private List<Map<String, String>> entries;
+	private int thread;
 
 
 	public Path(IProject project){
-		this.entries = new HashMap<String, String>();
+		this.entries = new ArrayList<Map<String,String>>();
+		this.entries.add(new HashMap<String, String>());
+		thread = 0;
 		this.project = project;
 	}
 	public Path(String path, String identifier, IProject project){
-		this.entries = new HashMap<String, String>();
-		entries.put(identifier, path);
+		this.entries = new ArrayList<Map<String,String>>();
+		this.entries.add(new HashMap<String, String>());
+		entries.get(thread).put(identifier, path);
 		this.project = project;
 	}
-	
-	public boolean exists(String identifier){
-		if(identifier == null || identifier == "")
-			return false;
-		return project.findMember(entries.get(identifier))!=null;
+
+	public boolean exists(String path){
+		return project.findMember(path.replaceAll("\"",	""))!=null;
 	}
 
+	public String getPath(String identifier){
+		String result;
+		for(Map<String,String> m : entries){
+			result = m.get(identifier);
+			if(result!=null)
+				return result;
+		}
+		
+		return "";
+	}
+	
 	public boolean addPath(String path){
 		return addPath(path, null);
 	}
-
+	
 	public boolean addPath(String path, String identifier){
 		if(identifier!=null)
-			this.entries.put(identifier, path);
-		return exists(entries.get(identifier));
-	}
-	public boolean addPath(String[] path, String identifier){
-		String p = "";
-		if(identifier!=null){
-			for(String part : path)
-				p += part;
-			this.entries.put(identifier, p);
-		}
-		return exists(identifier);
+			if(path.contains("\""))
+				this.entries.get(thread).put(identifier, path.replaceAll("\"", ""));
+			else
+				this.entries.get(thread).put(identifier, path);
+		return exists(entries.get(thread).get(identifier));
 	}
 	
+	public void newThread(){
+		entries.add(new HashMap<String, String>());
+		thread = entries.size()-1;
+	}
+	
+	public boolean prevThread(){
+		if(thread!=0){
+			thread--;
+			return true;
+		}
+		return false;
+	}
 	
 }
--- org.eclipse.emf.henshin.paths/src/org/eclipse/emf/henshin/paths/builder/PathParser.java
+++ org.eclipse.emf.henshin.paths/src/org/eclipse/emf/henshin/paths/builder/PathParser.java
@@ -36,15 +36,17 @@ public class PathParser{
 		int b = contents.read();
 		boolean ignoreComment = false;
 		boolean ignoreDoc = false;
+		line = 1;
 		while(b!=-1){
 			if((char)b==';' || (char)b=='{' || (char)b=='}'){
-				String temp = row;
+				findPath(row);
 				row = "";
-				if(temp.contains("\""))
-					findPath(temp);
 			}
 			else if((char)b == '\n'){
-				ignoreComment = false;
+				if(ignoreComment){
+					ignoreComment = false;
+					row = "";
+				}
 				line++;	
 			}
 			else if((char)b == '/' && prevb == '/') 
@@ -63,103 +65,73 @@ public class PathParser{
 	}
 
 	private void findPath(String row) {
-		String[] path = getMatchedStrings(row, "bank");//"\"[A-Za-z/\\.]+\"");
-		String[] identifier = getMatchedStrings(row, "([A-Za-z]+)(.*[\\(=])+","f =", "l ", "f.");
-		boolean found = paths.addPath(path, identifier[0]);
-		if(!found)
-			try {
-				reporter.error(new PathException("Error found!!!!", line));
-			} catch (SAXException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		String path = getMatchedStrings(row, "(\"[A-Za-z\\-\\_/\\.]+\"|\\((\\w|\\w+ *, *\\w+)*\\))").replaceAll("[\\(\\)]+", "");
+		String identifier = getMatchedStrings(row, "([A-Za-z]+)(.*[\\(=])+","f =", "l ", "f\\.", "f\\(");
+		if(!path.equals("") && !identifier.equals("")){
+			if(paths.exists(path))
+				paths.addPath(path, identifier);
+			else{
+				String prevIdentifier = getMatchedStrings(row, "([ =]\\w+\\.|\\(\\w+\\.)").replaceAll("[ .=\\(]", "");
+				String prevPath = paths.getPath(prevIdentifier);
+				if(!path.contains("\"")){
+					path = getPath(path);
+					if(path.length()==0) return;
+				}
+				else
+					path = path.replaceAll("\"", "");
+				path = prevPath + "/" + path;
+
+				if(paths.exists(path))
+					paths.addPath(path, identifier);
+				else
+					raiseError(true, "This Path ist failary: " + path);
 			}
+		}
+		else if(!identifier.equals("")){
+			path = getMatchedStrings(row, "\\(([A-Za-z]+)\\)");
+			path = paths.getPath(path.replaceAll("[\\(\\)]", ""));
+			if(path!=null && !path.equals(""))
+				raiseError(!paths.addPath(path, identifier), "Dont exsist: " + identifier + " = " + path);
+		}
 	}
 
-	private String[] getMatchedStrings(String text, String regex, String... split){
+	private String getPath(String path) {
+		String result = "";
+		String[] identifiers = path.split(" *, *");
+		for(String s : identifiers){
+			String temp = paths.getPath(s);
+			if(temp!=null)
+				result += result.length()==0?temp:":" + temp;
+		}
+		return result.contains(":")?null:result;
+	}
+
+	private String getMatchedStrings(String text, String regex, String... split){
 		Pattern patterns = Pattern.compile(regex);
 		Matcher m = patterns.matcher(text);
-		String[] result = new String[m.groupCount()];
-		for(int i=0; i<result.length; i++){
-			System.out.println("group=" + m.group());
-			result[i] = m.group(i);
+		String result = "";
+		String[] ssdsd = "run(".split("\\(");
+		for(int i=0; m.find(); i++){
+			String r = m.group(i);
 			if(split!=null)
-				for(String s : split)
-					result[i] = result[i].split(s.substring(1))[s.charAt(0)=='f'?0:result[i].split(s.substring(1)).length];
+				for(String s : split){
+					r = r.split(s.substring(1))[s.charAt(0)=='f'?0:r.split(s.substring(1)).length-1];
+				}
+			result += r;
 		}
 		return result;
 	}
-//	private void findPath2() {
-//
-//		line = 1;
-//		int b = 0;
-//		try {
-//			String text = "";
-//			boolean found = false;
-//			boolean check = false;
-//			char last = ' ';
-//			boolean ignore = false;
-//			while(b != -1){
-//				char c = (char) b;
-//				if(c == '\n')
-//					line++;
-//				else if(c == '+' && fullPath.isEmpty())
-//					plus = true;
-//				else if((c == '=' || c == '!' || c == ',' || c == '+' || c == '(' || c == ')' || c == '%') && found)
-//					ignore = true;
-//				else if(c == '"' && last != '\\')
-//					found = !found;
-//				else if(found && !ignore)
-//					text += c;
-//				else if((c == '}' || c == ';' || (c == ',' || (!found && c == ')'))) && !fullPath.isEmpty())
-//					check = true;
-//				else if(plus && c == ';')
-//					plus = false;
-//				
-//				
-//				if(text.length()!=0 && !found){
-//					text = text.replace("\\", "").replace("\\'", "").replace("\\n", "").replace("\\r", "");
-//					fullPath.add(text);
-//					text = "";
-//				}
-//				
-//				if(check){
-//					if(ignore)
-//						ignore = false;
-//					else
-//						if(!fullPath.get(fullPath.size()-1).endsWith("."))
-//							testPath();
-//					plus = false;
-//					fullPath = new ArrayList<String>();
-//					check = false;
-//				}
-//				last = c;
-//				b = contents.read();
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return;
-//		}
-//	}
-//
-//	private void testPath() throws SAXException, CoreException {
-//		String path = "";
-//		for (String s : fullPath)
-//			path+=s;
-//		if(path.contains("http://www.") || path.contains("http://") || path.contains("www.") || path.startsWith(".."))
-//			return;
-//		if(path.contains("/") || path.contains(".") || path.contains("\\")){
-//			
-//			if(!path.contains("/") && !path.contains("\\"))
-//				reporter.warning(new PathException("It could be a wrong Path, depending on how you using this String", line));
-//			else if(fullPath.size()>1 || plus)
-//				reporter.warning(new PathException("It could be a wrong Path, depending on the Parameters in the Path", line));
-//			else{
-//				if(found == null)
-//					if(path.contains("."))
-//						reporter.error(new PathException("File  \"" + path + "\"  in  \"" + pr.getName() + "\\\"  not exists", line));
-//					else
-//						reporter.error(new PathException("Directory  \"" + path + "\"  in  \"" + pr.getName() + "\\\"  not exists", line));
-//			}
-//		}
-//	}
+
+	private boolean raiseError(boolean found){
+		return raiseError(found, "");
+	}
+	private boolean raiseError(boolean error, String row){
+		if(error)
+			try {
+				reporter.error(new PathException(row, line));
+			} catch (SAXException e) {
+				e.printStackTrace();
+			}
+		return error;
+	}
 }
